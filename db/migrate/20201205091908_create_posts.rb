4f3c695e88133ac5a7e1669ff06ef79321f5a98a
tanimoto1995 <77666828+tanimoto1995@users.noreply.github.com>
2021-02-27 16:33:02 +0900

    既読機能のサーバーサイド実装

--- db/migrate/20201205091908_create_posts.rb
+++ db/migrate/20201205091908_create_posts.rb
@@ -6,4 +6,17 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.boolean :checked
     end
   end
+
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
